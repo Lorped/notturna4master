a1e97c55591c72f241b10338aa8490c788605565
lorped <lorped@gmail.com>
2024-07-28 18:16:19 +0200

    push

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -112,7 +112,29 @@ export class LoginPage implements OnInit {
       },
     };
 
+    PushNotifications.addListener('registration', (token: Token) => {
+      alert('Push registration success, token: ' + token.value);
+    });
 
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        //alert('Push received: ' + JSON.stringify(notification));
+      },
+    );
+
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        //alert('Push action performed: ' + JSON.stringify(notification));
+      },
+    );
+
+    this.router.navigate(['home']);
 
   /**** 
 
