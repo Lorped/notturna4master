a60dbd911cf40cd9b07598c0d966a504c66f14f4
lorped <lorped@gmail.com>
2024-07-28 21:14:13 +0200

    home parziale

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export class Utente {
+  nomepg = '';
+  id = 0;
+
+  constructor(nomepg: string, id: number) {
+    this.nomepg = nomepg;
+    this.id = id;
+  }
+
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -7,9 +19,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  listautenti: Array<Utente> = [];
+	pgscelto = 0;
+	selected = '';
+	oggetto = '';
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  godadi(){}
+  vedischeda(){}
+  inviamessaggio(){}
+  openbarcode(){}
+  godiablerie(){}
+  golistaoggetti(){}
+  logoutx(){}
 
 }
