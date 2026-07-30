50fd8072d2da573643cbade835865c70360e2af2
lorped <lorped@gmail.com>
2026-07-30 22:57:48 +0200

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { HttpClientModule } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withXhr,
+} from '@angular/common/http';
 
 import { PipesModule } from './pipes/pipes.module';
 import { FullOggetto, Condizione, Con } from './global';
@@ -16,8 +20,20 @@ import { FullOggetto, Condizione, Con } from './global';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, PipesModule ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, FullOggetto, Condizione, Con],
   bootstrap: [AppComponent],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    PipesModule 
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+    FullOggetto, 
+    Condizione, 
+    Con,
+    provideHttpClient(withXhr(), withInterceptorsFromDi()),
+  ],
+  
 })
 export class AppModule {}
