df5c6d52cb8512e90a98d145b24c5565ffd5823f
lorped <lorped@gmail.com>
2026-07-30 21:24:51 +0200

    angular 21

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    applicationProviders: [provideZoneChangeDetection()],
+  })
+  .catch((err) => console.log(err));
